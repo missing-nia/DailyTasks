374135cbf3342908decee4a8c86dd37a24fb74ae
acprado <ardenprado@yahoo.com>
2021-12-27 16:41:16 -0800

    fixed formatting

--- main.py
+++ main.py
@@ -48,34 +48,34 @@ async def addtask(ctx, task):
 		
 @addtask.error
 async def addtask_error(ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			await ctx.send('Invalid arguments! Please use ?help for further information')
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send('Invalid arguments! Please use ?help for further information')
 		
-@bot.command(brief="Removes a specified task")
+@bot.command(brief = "Removes a specified task")
 async def removetask(ctx, task):
-		await ctx.send('Removed task \"{}\"'.format(task))
+	await ctx.send('Removed task \"{}\"'.format(task))
 		
 @removetask.error
 async def removetask_error(ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			await ctx.send('Invalid arguments! Please use ?help for further information')
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send('Invalid arguments! Please use ?help for further information')
 		
-@bot.command(brief="Checks in to a specified task")
+@bot.command(brief = "Checks in to a specified task")
 async def checkin(ctx, task, time_in_minutes: int):
-		await ctx.send('Checked in successfully. {} minutes logged for "{}"'.format(time_in_minutes, task))
+	await ctx.send('Checked in successfully. {} minutes logged for "{}"'.format(time_in_minutes, task))
 		
 @checkin.error
 async def checkin_error(ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			await ctx.send('Invalid arguments! Please use ?help for further information')
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send('Invalid arguments! Please use ?help for further information')
 		
-@bot.command(brief="Lists all registered tasks")
+@bot.command(brief = "Lists all registered tasks")
 async def listtasks(ctx):
-		await ctx.send('Current tasks are:')
+	await ctx.send('Current tasks are:')
 		
 @listtasks.error
 async def listtasks_error(ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			await ctx.send('Invalid arguments! Please use ?help for further information')
+	if isinstance(error, commands.MissingRequiredArgument):
+		await ctx.send('Invalid arguments! Please use ?help for further information')
 		
 bot.run(DISCORD_TOKEN)
